9db2d56b83abd15cfeca2aeb68ae0641726ef30e
Zachery Wampler <ztw4322@rit.edu>
2025-11-19 14:49:50 -0500

    Project 2 start

---
+++ server/router.js
@@ -0,0 +1,20 @@
+const controllers = require('./controllers');
+const mid = require('./middleware');
+
+const router = (app) => {
+  app.get('/getDomos', mid.requireLogin, controllers.Domo.getDomos);
+
+  app.get('/login', mid.requiresSecure, mid.requireLogout, controllers.Account.loginPage);
+  app.post('/login', mid.requiresSecure, mid.requireLogout, controllers.Account.login);
+
+  app.post('/signup', mid.requiresSecure, mid.requireLogout, controllers.Account.signup);
+
+  app.get('/logout', mid.requireLogin, controllers.Account.logout);
+
+  app.get('/maker', mid.requireLogin, controllers.Domo.makerPage);
+  app.post('/maker', mid.requireLogin, controllers.Domo.makeDomo);
+
+  app.get('/', mid.requiresSecure, mid.requireLogout, controllers.Account.loginPage);
+};
+
+module.exports = router;
